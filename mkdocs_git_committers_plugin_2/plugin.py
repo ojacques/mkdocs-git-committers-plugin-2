2d34cc25b94f1423922b5f7abca973905344b5f8
Olivier Jacques <ojacques2@gmail.com>
2022-09-07 23:58:11 +0200

    Add cache for page authors

--- mkdocs_git_committers_plugin_2/plugin.py
+++ mkdocs_git_committers_plugin_2/plugin.py
@@ -27,6 +27,7 @@ class GitCommittersPlugin(BasePlugin):
         ('branch', config_options.Type(str, default='master')),
         ('docs_path', config_options.Type(str, default='docs/')),
         ('enabled', config_options.Type(bool, default=True)),
+        ('cache_dir', config_options.Type(str, default='.cache/plugin/git-committers')),
     )
 
     def __init__(self):
@@ -34,6 +35,8 @@ class GitCommittersPlugin(BasePlugin):
         self.branch = 'master'
         self.enabled = True
         self.authors = dict()
+        self.cache_page_authors = dict()
+        self.cache_date = ''
 
     def on_config(self, config):
         self.enabled = self.config['enabled']
@@ -61,17 +64,26 @@ class GitCommittersPlugin(BasePlugin):
                 # Use the last commit and get the date
                 last_commit_date = time.strftime("%Y-%m-%d", time.gmtime(c.authored_date))
 
+        # File not committed yet
+        if last_commit_date == "":
+            last_commit_date = datetime.now().strftime("%Y-%m-%d")
+            return [], last_commit_date
+
+        # Try to leverage the cache
+        if self.cache_date and time.strptime(last_commit_date, "%Y-%m-%d") < time.strptime(self.cache_date, "%Y-%m-%d"):
+            return self.cache_page_authors[path]['authors'], self.cache_page_authors[path]['last_commit_date']
+
         url_contribs = self.githuburl + self.config['repository'] + "/contributors-list/" + self.config['branch'] + "/" + path
-        LOG.info("Fetching contributors for " + path)
+        LOG.info("git-committers: fetching contributors for " + path)
         LOG.debug("   from " + url_contribs)
         authors=[]
         try:
             response = requests.get(url_contribs)
             response.raise_for_status()
         except HTTPError as http_err:
-            LOG.error(f'HTTP error occurred: {http_err}\n(404 is normal if file is not on GitHub yet or Git submodule)')
+            LOG.error(f'git-committers: HTTP error occurred: {http_err}\n(404 is normal if file is not on GitHub yet or Git submodule)')
         except Exception as err:
-            LOG.error(f'Other error occurred: {err}')
+            LOG.error(f'git-committers: Other error occurred: {err}')
         else:
             html = response.text
             # Parse the HTML
@@ -86,6 +98,8 @@ class GitCommittersPlugin(BasePlugin):
                 avatar = img_tags[0]['src']
                 avatar = re.sub(r'\?.*$', '', avatar)
                 authors.append({'login':login, 'name': name, 'url': url, 'avatar': avatar})
+            # Update global cache_page_authors
+            self.cache_page_authors[path] = {'last_commit_date': last_commit_date, 'authors': authors}
 
         return authors, last_commit_date
 
@@ -104,3 +118,20 @@ class GitCommittersPlugin(BasePlugin):
         self.total_time += (end - start)
 
         return context
+
+    def on_post_build(self, config):
+        LOG.info("git-committers: saving page authors cache file")
+        json_data = json.dumps({'cache_date': datetime.now().strftime("%Y-%m-%d"), 'page_authors': self.cache_page_authors})
+        os.makedirs(self.config['cache_dir'], exist_ok=True)
+        f = open(self.config['cache_dir'] + "/page-authors.json", "w")
+        f.write(json_data)
+        f.close()
+
+    def on_pre_build(self, config):
+        if os.path.exists(self.config['cache_dir'] + "/page-authors.json"):
+            LOG.info("git-committers: found page authors cache file - loading it")
+            f = open(self.config['cache_dir'] + "/page-authors.json", "r")
+            cache = json.loads(f.read())
+            self.cache_date = cache['cache_date']
+            self.cache_page_authors = cache['page_authors']
+            f.close()
